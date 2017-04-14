21ba842cc585b73b257cfe30fd73f82d196aaf31
Jeffrey Gordon <jeffreyrgordon@gmail.com>
2017-04-14 00:30:58 -0600

    Update mode-verilog.js

--- src-min-noconflict/mode-verilog.js
+++ src-min-noconflict/mode-verilog.js
@@ -3,7 +3,7 @@
     var r = e("../lib/oop"),
         i = e("./text_highlight_rules").TextHighlightRules,
         s = function() {
-            var e = "if|endif",
+            var e = "if|endif|else if",
                 t = "use",
                 n = "near|with|around|mention",
                 r = this.createKeywordMapper({
@@ -39,7 +39,7 @@
                     regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
                 }, {
                     token: "keyword.operator",
-                    regex: "\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|="
+                    regex: "\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|=|or"
                 }, {
                     token: "paren.lparen",
                     regex: "[\\(]"
